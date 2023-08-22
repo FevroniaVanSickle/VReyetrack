61408c562fb5950623c4e65d3704aba4e16f7833
FevroniaVanSickle <nia13marie@gmail.com>
2023-08-22 10:21:55 -0700

    entrypoint

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         "dev": ["pytest>=7.0", "twine>=4.0.2"]
     },
     python_requires=">=3.10",
-    entry_points={"console_scripts": ["rolloutmethod= RollOutGui.rolloutmethod:main"]},
+    entry_points={
+    "console_scripts": ["rolloutmethod=RollOutGui.src.rolloutmethod:main"]
+},
 
 )
