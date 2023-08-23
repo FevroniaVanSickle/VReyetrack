09eea0956aa4acb2e645af491005b27c23295576
FevroniaVanSickle <nia13marie@gmail.com>
2023-08-22 22:55:56 -0700

    entry points

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ setup(
                 "License :: OSI Approved :: MIT License",
                 "Programming Language :: Python :: 3.11",
                 "Operating System :: OS Independent"],
-    install_requires=["matplotlib >= 3.5", "opencv-python >= 4.5", "shapely >= 2", "Pillow >= 9.5", "labelme >= 5.0"],
+    install_requires=["matplotlib >= 3.5", "opencv-python >= 4.5", "shapely >= 2", "labelme >= 5.0", "Pillow >= 9.5"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"]
     },
     python_requires=">=3.10",
-    entry_points={
-    "console_scripts": ["rolloutmethod=RollOutGui.src.rolloutmethod:main"]
-},
+    # entry_points={
+    # "console_scripts": ["rolloutmethod=RollOutGui.src.rolloutmethod:main"]
+    entry_points={"console_scripts": ["rolloutmethod=app.RollOutGui.src.rolloutmethod:main"]}
 
 )
