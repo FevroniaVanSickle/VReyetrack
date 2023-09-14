0e962c4cdf622d3ca67f0d1464ca624aa2a74523
FevroniaVanSickle <nia13marie@gmail.com>
2023-09-14 14:38:11 -0400

    build

---
+++ build/lib/src/main.py
@@ -0,0 +1,8 @@
+from RollOutGui.src.rolloutmethod import RollOutMethodInterface
+
+def main():
+    appROM = RollOutMethodInterface()
+    appROM.mainloop()
+
+if __name__ == "__main__":
+    main()
