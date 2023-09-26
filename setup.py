42b4d81491ac592613bcaa1eeec0a7f3ebb34db7
FevroniaVanSickle <nia13marie@gmail.com>
2023-09-26 13:44:32 -0400

    added pandas to setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
                 "License :: OSI Approved :: MIT License",
                 "Programming Language :: Python :: 3.11",
                 "Operating System :: OS Independent"],
-    install_requires=["labelme >= 5.3", "tk", "matplotlib >= 3.5", "opencv-python >= 4.5", "shapely >= 2", "Pillow >= 9.5"],
+    install_requires=["labelme >= 5.3", "pandas >= 2", "tk", "matplotlib >= 3.5", "opencv-python >= 4.5", "shapely >= 2", "Pillow >= 9.5"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"]
     },
