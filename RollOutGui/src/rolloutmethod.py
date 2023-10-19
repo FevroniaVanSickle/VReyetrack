2e8a997058ac9ddc1eebf7d754552ee311d33333
FevroniaVanSickle <nia13marie@gmail.com>
2023-10-18 22:08:40 -0400

    updated process.py path

--- RollOutGui/src/rolloutmethod.py
+++ RollOutGui/src/rolloutmethod.py
@@ -447,8 +447,8 @@ class TadaPage(tk.Frame):
 
         #make sure correct Data folder is chosen 
         if (self.dataFolderPath.__contains__("Data")):
-            print("calling passing data folder to process.py at line 450")
-            subprocess.run(["python3", "RollOutGui/src/process.py", self.dataFolderPath])
+            print("passing data folder to process.py at line 450")
+            subprocess.run(["python3", "VReyetrack-main/RollOutGui/src/process.py", self.dataFolderPath])
         else:
             print("Please select the data folder ")
             # sys.exit(1)
